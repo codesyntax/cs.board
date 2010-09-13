f97960b49843053c2384d05d8b0721b85ca1bb5e
erral <mlarreategi@codesyntax.com>
2010-09-13 12:54:53 +0000

    dependency
    svn path=/cs.board/trunk/; revision=35861

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(name='cs.board',
       zip_safe=False,
       install_requires=['setuptools',
                         # -*- Extra requirements: -*-
+                        'archetypes.multifile',
                         ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
