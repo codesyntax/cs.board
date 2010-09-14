398cff430e9f33f7784bc01beb835501c52b1d09
erral <mlarreategi@codesyntax.com>
2010-09-14 06:55:49 +0000

    view for document
    svn path=/cs.board/trunk/; revision=35922

---
+++ cs/board/browser/documentview.py
@@ -0,0 +1,38 @@
+from zope.interface import implements, Interface
+from Acquisition import aq_inner
+
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+from cs.board import boardMessageFactory as _
+
+
+class IDocumentView(Interface):
+    """
+    Document view interface
+    """
+    def files():
+        """ Get files of this document """
+
+class DocumentView(BrowserView):
+    """
+    Document browser view
+    """
+    implements(IDocumentView)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+
+    @property
+    def portal(self):
+        return getToolByName(self.context, 'portal_url').getPortalObject()
+
+    def files(self):
+        context = aq_inner(self.context)
+        return self.portal_catalog(path='/'.join(context.getPhysicalPath()),
+                                   portal_type='File')
