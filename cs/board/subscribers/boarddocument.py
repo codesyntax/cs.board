95b9219a47338b5e0eae48c8ce93dfa8287fb549
erral <mlarreategi@codesyntax.com>
2010-09-14 13:23:09 +0000

    subscriber to publish documents in all languages
    svn path=/cs.board/trunk/; revision=35964

--- cs/board/subscribers/boarddocument.py
+++ cs/board/subscribers/boarddocument.py
@@ -1,20 +1,32 @@
-from Acquisition import aq_parent
-from Products.LinguaPlone.public import AlreadyTranslated
+from Products.CMFCore.utils import getToolByName
 
-def translateFiles(object, event):
-    parent = aq_parent(object)
-    if parent.portal_type == 'BoardDocument':
-        translations = parent.getTranslations()
-        for lang, translated in translations.items():
-            if lang != object.Language():
-                try:
-                    object.addTranslation(language=lang,
-                                          title=object.Title())
-                except AlreadyTranslated:
-                    from logging import getLogger
-                    log = getLogger('cs.board.subscriber.boarddocument')
-                    log.info('Document already translated')
+def publishDocument(object, event):
+    wtool = getToolByName(object, 'portal_workflow')
+    if event.action == 'publish':
+        for lang, translated in object.getTranslations().items():
+            status = translated[1]            
+            if status == 'private':
+                trans_obj = translated[0]
+                wtool.doActionFor(trans_obj, 'publish')
+            else:
+                from logging import getLogger
+                log = getLogger('cs.board.subscribers.boarddocument')
+                log.info('Already published')
 
+    elif event.action == 'retract':
+        for lang, translated in object.getTranslations().items():
+            status = translated[1]            
+            if status == 'published':
+                trans_obj = translated[0]
+                wtool.doActionFor(trans_obj, 'retract')
+            else:
+                from logging import getLogger
+                log = getLogger('cs.board.subscribers.boarddocument')
+                log.info('Already private')
+        
+    else:
+        from logging import getLogger
+        log = getLogger('cs.board.subscribers.boarddocument')
+        log.info('status == %s' % status)
 
-    
         
