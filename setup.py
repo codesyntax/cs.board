e52139e39208057ccb57f94fb8736f236f6fad54
erral <mlarreategi@codesyntax.com>
2010-09-20 09:15:18 +0000

    dependencies
    svn path=/cs.board/trunk/; revision=36213

--- setup.py
+++ setup.py
@@ -56,6 +56,8 @@ setup(name='cs.board',
       zip_safe=False,
       install_requires=['setuptools',
                         # -*- Extra requirements: -*-
+                        'z3c.soap',
+                        'soaplib==0.8.1',
                         ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
