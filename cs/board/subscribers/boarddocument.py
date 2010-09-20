438e135aaf308550f6abbdd00360504c83a4f418
erral <mlarreategi@codesyntax.com>
2010-09-20 10:29:18 +0000

    Add auto-translation hook for BoardDocuments
    svn path=/cs.board/trunk/; revision=36229

--- cs/board/subscribers/boarddocument.py
+++ cs/board/subscribers/boarddocument.py
@@ -1,4 +1,21 @@
+from Acquisition import aq_parent
+from Products.LinguaPlone.public import AlreadyTranslated
 from Products.CMFCore.utils import getToolByName
+
+def translateDocuments(object, event):
+    parent = aq_parent(object)
+    if parent.portal_type == 'Board':
+        translations = parent.getTranslations()
+        for lang, translated in translations.items():
+            if lang != object.Language():
+                try:
+                    object.addTranslation(language=lang,
+                                          title=object.Title())
+                except AlreadyTranslated:
+                    from logging import getLogger
+                    log = getLogger('cs.board.subscribers.boarddocument')
+                    log.info('BoardDocument already translated')
+
 
 def publishDocument(object, event):
     wtool = getToolByName(object, 'portal_workflow')
