fc4a114300458536559099f5dc66976e3fcf0035
erral <mlarreategi@codesyntax.com>
2011-03-17 08:23:43 +0000

    new list of docs
    svn path=/cs.board/trunk/; revision=43800

--- cs/board/browser/boardview.py
+++ cs/board/browser/boardview.py
@@ -4,8 +4,8 @@ from Acquisition import aq_inner
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
-from cs.board import boardMessageFactory as _
-
+from DateTime import DateTime
+from plone.memoize.view import memoize
 
 class IBoardView(Interface):
     """
@@ -15,10 +15,11 @@ class IBoardView(Interface):
     def getPublishedDocuments():
         """ Published documents on the board """
 
+    def getPublishedExpiredDocuments():
+        """ Published but expired documents on the board """
 
     def getPrivateDocuments():
         """ Private documents on the board """
-
 
 class BoardView(BrowserView):
     """
@@ -38,18 +39,35 @@ class BoardView(BrowserView):
     def portal(self):
         return getToolByName(self.context, 'portal_url').getPortalObject()
 
+    @memoize
     def getPublishedDocuments(self):
         context = aq_inner(self.context)
         return self.portal_catalog(path='/'.join(context.getPhysicalPath()),
+                                   effective={'query': DateTime(),
+                                              'range': 'max'},
+                                   expires={'query': DateTime(),
+                                            'range': 'min'},
                                    sort_on='effective',
                                    sort_order='reverse',
                                    portal_type='BoardDocument',
                                    review_state='published')
 
+    @memoize
+    def getPublishedExpiredDocuments(self):
+        context = aq_inner(self.context)
+        return self.portal_catalog(path='/'.join(context.getPhysicalPath()),
+                                   expires={'query': DateTime(),
+                                            'range': 'max'},
+                                   sort_on='effective',
+                                   sort_order='reverse',
+                                   portal_type='BoardDocument',
+                                   review_state='published')
 
+    @memoize
     def getPrivateDocuments(self):
         context = aq_inner(self.context)
         return self.portal_catalog(path='/'.join(context.getPhysicalPath()),
                                    sort_on='created',
                                    portal_type='BoardDocument',
                                    review_state='private')
+
